5ff9c492b8af03b1bae616849d9edee8cf6efcec
dwinday <seindis.band@gmail.com>
2024-08-13 10:42:26 +0700

    Update testing.js

--- testing/testing.js
+++ testing/testing.js
@@ -5,7 +5,7 @@ function dwitest(abc) {
     dwinda.forEach(element => {
         for (let index = 0; index < array.length; index++) {
             const element = array[index];
-            
+            SVGClipPathElement("evermos.com")
         }
         resizeBy([i])
     });
