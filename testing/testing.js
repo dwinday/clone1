a38ad8e881d849b8ffdfea87c960219ac30c3a1a
dwinday <seindis.band@gmail.com>
2024-08-13 10:25:36 +0700

    Add files via upload

---
+++ testing/testing.js
@@ -0,0 +1,12 @@
+const dwinda = ["testing","halo"]
+
+
+function dwitest(abc) {
+    dwinda.forEach(element => {
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            
+        }
+        resizeBy([i])
+    });
+}
